9ea63cec865beafe3fef021ab53d941be2b6ed2a
Alejandra lopez pineda <al.lopezpineda@gmail.com>
2021-03-11 09:55:50 +0100

    Updating project.

---
+++ EGym.Logic/Repository/RAreaGym.cs
@@ -0,0 +1,40 @@
+﻿using EGym.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGym.Logic.Repository
+{
+    class RAreaGym
+    {
+        /*List<AreaGym> areaGym { get; set; }
+        public RAreaGym()
+        {
+            areaGym = new List<AreaGym>()
+            {
+                new AreaGym
+                {
+                    Id = 1,
+                    Name = "ROOM ZUMBA"
+                },
+                new AreaGym
+                {
+                    Id = 2,
+                    Name = "ROOM PILATES"
+                }
+            };
+        }
+
+        public List<AreaGym> Get()
+        {
+            return areaGym;
+        }
+
+        public AreaGym Get(int id)
+        {
+            return areaGym.SingleOrDefault(a => a.Id == id);
+        }*/
+    }
+}
